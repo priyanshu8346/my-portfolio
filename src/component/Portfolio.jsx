2d174656435118b6a8dc04bb23bcc7c9ac2fd9f9
Priyanshu Agrawal <priyanshuagrawal419@gmail.com>
2023-11-04 03:53:35 +0530

    final

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
+import pocketnews from "../assets/portfolio/pocketnews.jpg";
+import textPlay from "../assets/portfolio/text-play.jpg";
 import project1 from "../assets/portfolio/project 1.jpeg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+import scintillation from "../assets/portfolio/Scintillation.jpeg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
+import EDC from "../assets/portfolio/EDC.jpeg";
 
 const Portfolio = () => {
 
@@ -13,26 +13,38 @@ const Portfolio = () => {
         {
             id:1,
             address:project1,
+            name:"Tijori",
+            repo:"https://github.com/priyanshu8346/Tijori"
         },
         {
             id:2,
-            address:arrayDestruct,
+            address:pocketnews,
+            name:"PocketNews",
+            repo:"https://github.com/priyanshu8346/PocketNews"
         },
         {
             id:3,
-            address:reactSmooth,
+            address:textPlay,
+            name:"Text-Play",
+            repo:"https://github.com/priyanshu8346/text-play"
         },
         {
             id:4,
             address:reactWeather,
+            name:"Weather App",
+            repo:"https://github.com/priyanshu8346/weather-app"
         },
         {
             id:5,
-            address:navbar,
+            address:scintillation,
+            name:"Scintillation",
+            repo:"https://github.com/priyanshu8346/scintillation2023"
         },
         {
             id:6,
-            address:installNode,
+            address:EDC,
+            name:"EDC",
+            repo:"https://github.com/priyanshu8346/edc-1"
         }
     ]
     
@@ -49,13 +61,14 @@ const Portfolio = () => {
           <p className="my-6">Checkout some of my work here</p>
         </div>
         <div className="pb-32 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[3rem]" >
-        {links.map(({id, address})=>{  
+        {links.map(({id, address,name,repo})=>{  
              return (
                 <div key={id} id="cards_experience" className="shadow-md shadow-gray-600 rounded-lg" >
                     <img id="experience" src={address} alt="Project" style={{"height":"8rem", "width":"100%"}}/>
-                    <div className="flex items-center justify-between ">
-                        <button className="w-1/2 px-6 py-2 duration-300 hover:scale-105 text-lg">Demo</button>
-                        <button className="w-1/2 px-6 py-2 duration-300 hover:scale-105 text-lg">Code</button>
+                    <div className="flex items-center justify-center ">
+                    <a href={repo} target="_parent">
+                        <button className="px-6 py-2 duration-300 hover:scale-105 text-lg">{name}</button>
+                        </a>
                     </div>
                 </div>
             );
